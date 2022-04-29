1b0e16554d121670ff3aa923a822428bd258f2dc
nnamm <t.nnamm@gmail.com>
2022-04-30 01:01:28 +0900

    changed fish theme color scheme

--- fish/conf.d/theme.fish
+++ fish/conf.d/theme.fish
@@ -1,5 +1,6 @@
 # Override oh-my-fish/theme-bobthefish config
 # https://github.com/oh-my-fish/theme-bobthefish
+set -g theme_color_scheme nord
 set -g theme_display_date no
 set -g theme_date_format "+%H:%M:%S"
 set -g theme_date_timezone Asia/Tokyo
