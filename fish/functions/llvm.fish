7a5dd895060a4c902835317681e21381e74955a7
Takashi Hanamura <t.nnamm@gmail.com>
2022-05-21 00:38:43 +0900

    Cpp dev for mac (#5)
    * Comment out the POETRY setting as it seems unnecessary
    
    * C/C++ lang env was setup for mac

--- fish/functions/llvm.fish
+++ fish/functions/llvm.fish
@@ -1,9 +1,11 @@
 function llvm -d "set the llvm path&variables"
     switch (uname)
     case Darwin
-        echo MAC
+        set -g PATH "/usr/local/opt/llvm/bin" $PATH
+        set -gx LDFLAGS "-L/usr/local/opt/llvm/lib"
+        set -gx CPPFLAGS "-I/usr/local/opt/llvm/include"
     case Linux
-        set -gx LDFLAGS -L/usr/lib/llvm/lib
-        set -gx CPPFLAGS -I/usr/lib/llvm/include
+        set -gx LDFLAGS "-L/usr/lib/llvm/lib"
+        set -gx CPPFLAGS "-I/usr/lib/llvm/include"
     end
 end
