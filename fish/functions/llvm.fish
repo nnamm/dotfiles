cf5d880047086abf1996a66303225a798f4a7582
nnamm <t.nnamm@gmail.com>
2022-05-21 00:01:25 +0900

    fix C/C++ lang's path options

--- fish/functions/llvm.fish
+++ fish/functions/llvm.fish
@@ -3,7 +3,7 @@ function llvm -d "set the llvm path&variables"
     case Darwin
         echo MAC
     case Linux
-        set -x LDFLAGS -L/usr/lib/llvm/lib
-        set -x CPPFLAGS -I/usr/lib/llvm/include
+        set -gx LDFLAGS -L/usr/lib/llvm/lib
+        set -gx CPPFLAGS -I/usr/lib/llvm/include
     end
 end
