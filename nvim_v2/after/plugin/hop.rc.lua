0ba2ee6e9a856496e5d5d05e374ca24d4b7779ef
nnamm <t.nnamm@gmail.com>
2022-09-07 12:17:51 +0900

    add command for hop

--- nvim_v2/after/plugin/hop.rc.lua
+++ nvim_v2/after/plugin/hop.rc.lua
@@ -4,3 +4,4 @@ if (not status) then return end
 hop.setup()
 
 vim.api.nvim_set_keymap('n', '<leader>j', ":HopWord<CR>", { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>c', ":HopChar2<CR>", { silent = true })
