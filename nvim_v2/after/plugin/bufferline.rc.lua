644101ff3ea1460e7ce922637556693331ef4967
nnamm <t.nnamm@gmail.com>
2022-09-09 15:41:47 +0000

    change bufferline settings

--- nvim_v2/after/plugin/bufferline.rc.lua
+++ nvim_v2/after/plugin/bufferline.rc.lua
@@ -4,18 +4,20 @@ if (not status) then return end
 bufferline.setup({
   options = {
     mode = "buffers",
-    separator_style = { '|', '|' },
-    always_show_bufferline = true,
+    indicator = { style = "none" },
+    -- diagnostics = "nvim_lsp",
+    color_icons = true,
+    show_buffer_icons = true,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    color_icons = true
+    separator_style = { "|", "|" },
+    always_show_bufferline = true,
   },
   highlights = {
     fill = {
-      bg = '#bf616a'
+      bg = '#2e3440'
     },
     background = {
-      --fg = '#3b4252',
       fg = '#81a1c1',
       bg = '#2e3440'
     },
@@ -26,8 +28,17 @@ bufferline.setup({
     buffer_selected = {
       fg = '#ebcb8b',
       bg = '#3b4252',
-      --bg = '#2e3440',
-      bold = true
+      bold = true,
+      italic = false
+    },
+    modified = {
+      bg = '#2e3440'
+    },
+    modified_visible = {
+      bg = '#2e3440'
+    },
+    modified_selected = {
+      bg = '#3b4252',
     },
     separator = {
       fg = '#81a1c1',
