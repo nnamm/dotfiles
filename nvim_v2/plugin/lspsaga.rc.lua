51341715f85cdd39adadbed0d6eda11ce445e611
nnamm <t.nnamm@gmail.com>
2022-10-02 21:05:49 +0900

    change lspsaga settings

--- nvim_v2/plugin/lspsaga.rc.lua
+++ nvim_v2/plugin/lspsaga.rc.lua
@@ -2,7 +2,10 @@ local status, saga = pcall(require, "lspsaga")
 if (not status) then return end
 
 saga.init_lsp_saga {
-  server_filetype_map = {}
+  code_action_lightbulb = {
+    enable = false
+  },
+  server_filetype_map = {},
 }
 
 -- Ref: https://github.com/glepnir/lspsaga.nvim
@@ -10,6 +13,6 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-j>', '<cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gl', '<cmd>Lspsaga lsp_finder<CR>', opts)
-vim.keymap.set('n', 'gp', '<cmd>Lspsaga preview_definition<CR>', opts)
+vim.keymap.set('n', 'gp', '<cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', '<Leader>a', '<cmd>Lspsaga code_action<CR>', opts)
 vim.keymap.set('n', '<Leader>r', '<cmd>Lspsaga rename<CR>', opts)
