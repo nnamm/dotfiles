05c46ec3762b8a05f4c21e08b127695479d2ff99
nnamm <t.nnamm@gmail.com>
2022-10-18 23:07:56 +0900

    change sources settings

--- nvim_v2/plugin/null-ls-rc.lua
+++ nvim_v2/plugin/null-ls-rc.lua
@@ -3,12 +3,12 @@ if (not status) then return end
 
 local sources = {
   -- diagnostics
-  -- null_ls.builtins.diagnostics.mypy,  -- Requires machine resources to use
-  -- null_ls.builtins.diagnostics.pylint, -- Requires machine resources to use
+  null_ls.builtins.diagnostics.mypy, -- Requires machine resources to use
+  null_ls.builtins.diagnostics.pylint, -- Requires machine resources to use
   null_ls.builtins.diagnostics.pyproject_flake8,
-  null_ls.builtins.diagnostics.eslint_d.with({
-    diagnostics_format = '[eslint] #{m}\n(#{c})'
-  }),
+  -- null_ls.builtins.diagnostics.eslint_d.with({
+  --   diagnostics_format = '[eslint] #{m}\n(#{c})'
+  -- }),
 
   -- formatting
   null_ls.builtins.formatting.gofmt,
@@ -44,19 +44,3 @@ null_ls.setup({
     end
   end,
 })
-
--- local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
--- ---@diagnostic disable-next-line: redundant-parameter
--- null_ls.setup {
---   sources = sources,
---   on_attach = function(client, bufnr)
---     if client.server_capabilities.documentFormattingProvider then
---       vim.api.nvim_clear_autocmds { buffer = 0, group = augroup_format }
---       vim.api.nvim_create_autocmd("BufWritePre", {
---         group = augroup_format,
---         buffer = 0,
---         callback = function() vim.lsp.buf.formatting_seq_sync() end
---       })
---     end
---   end,
--- }
