5d0e5b40164fe758223ef008cf9e7b0c32c4bc96
nnamm <t.nnamm@gmail.com>
2024-09-07 22:48:25 +0900

    add tree-sitter ensure_installed

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "css",
+        "go",
+        "html",
+        "javascript",
+        "svelte",
+        "typescript",
+      },
+    },
+  },
+}
