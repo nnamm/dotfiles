d17f226b3a28771fc96a0b05eb866cce6f8f2b14
nnamm <t.nnamm@gmail.com>
2024-11-07 23:33:58 +0900

    added setting for c,cpp

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -29,6 +29,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.overseer" },
     { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
