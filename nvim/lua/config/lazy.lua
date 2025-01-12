d225764a4c9afd439352e60ab31e244895a6c570
nnamm <t.nnamm@gmail.com>
2025-01-12 23:46:14 +0900

    disabled for uv

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -31,7 +31,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.overseer" },
     { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "lazyvim.plugins.extras.editor.telescope" },
-    { import = "lazyvim.plugins.extras.formatting.black" },
+    -- { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.cmake" },
