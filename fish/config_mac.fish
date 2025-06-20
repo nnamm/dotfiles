57c32a5852f443ef9ab3f7fcb36b3a7d30a73ba3
nnamm <t.nnamm@gmail.com>
2025-06-21 01:31:38 +0900

    fix: 🐛 installed ghq

--- fish/config_mac.fish
+++ fish/config_mac.fish
@@ -1,4 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+
+    # gqh + fzf interactive
+    function ghq-fzf
+        set -l src (ghq list | fzf --height 50% --preview "bat --color=always --line-range :80 (ghq root)/{}/README.*")
+        if test -n "$src"
+            cd (ghq root)/$src
+            commandline -f execute
+        end
+    end
+    # Bind ctrl+g to ghq-fzf function
+    bind \cg ghq-fzf
 end
