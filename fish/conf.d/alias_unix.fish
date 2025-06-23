9ba09d11f160fedf2d47071110e00ca0b0362294
nnamm <t.nnamm@gmail.com>
2025-06-23 14:19:48 +0900

    WSL settings

--- fish/conf.d/alias_unix.fish
+++ fish/conf.d/alias_unix.fish
@@ -14,6 +14,10 @@ alias gk='git checkout -b $1'
 alias gl='git log --graph --oneline'
 alias gp='git pull'
 alias gs='git status -sb'
+alias cz="czg"
+alias lg='lazygit'
+alias pr='cd (git rev-parse --show-toplevel)'
+
 alias doc='docker'
 alias dcc='docker compose'
 
