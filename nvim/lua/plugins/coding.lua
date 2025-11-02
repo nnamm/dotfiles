2649d388b9bfa71c62a359b2df31f440d50878a9
nnamm <t.nnamm@gmail.com>
2025-11-02 18:08:19 +0900

    key remap

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -44,18 +44,6 @@ return {
   -- },
 
   -- copilot
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   cmd = "Copilot",
-  --   build = ":Copilot auth",
-  --   opts = {
-  --     suggestion = { enabled = true },
-  --     panel = { enabled = true },
-  --     filetypes = {
-  --       ["*"] = false,
-  --     },
-  --   },
-  -- },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     opts = {
@@ -70,49 +58,49 @@ return {
       prompts = {
         Explain = {
           prompt = "/COPILOT_EXPLAIN コードを日本語で説明してください",
-          mapping = "<leader>ce",
+          mapping = "<leader>cce",
           description = "コードの説明",
         },
         Review = {
           prompt = "/COPILOT_REVIEW コードを日本語でレビューしてください。",
-          mapping = "<leader>cr",
+          mapping = "<leader>ccr",
           description = "コードのレビュー",
         },
         Fix = {
           prompt = "/COPILOT_FIX このコードには問題があります。バグを修正したコードを表示してください。説明は日本語でお願いします。",
-          mapping = "<leader>cf",
+          mapping = "<leader>ccf",
           description = "バグコードの修正",
         },
         Optimize = {
           prompt = "/COPILOT_REFACTOR 選択したコードを最適化し、パフォーマンスと可読性を向上させてください。説明は日本語でお願いします。",
-          mapping = "<leader>co",
+          mapping = "<leader>cco",
           description = "コードの最適化",
         },
         Docs = {
           prompt = "/COPILOT_GENERATE 選択したコードに関するドキュメントコメントを日本語で生成してください。",
-          mapping = "<leader>cd",
+          mapping = "<leader>ccd",
           description = "コードのドキュメント作成",
         },
         Tests = {
           prompt = "/COPILOT_TESTS 選択したコードの詳細なユニットテストを書いてください。説明は日本語でお願いします。",
-          mapping = "<leader>ct",
+          mapping = "<leader>cct",
           description = "テストコード作成",
         },
         FixDiagnostic = {
           prompt = "コードの診断結果に従って問題を修正してください。修正内容の説明は日本語でお願いします。",
-          mapping = "<leader>cd",
+          mapping = "<leader>ccd",
           description = "コードの修正",
           selection = require("CopilotChat.select").diagnostics,
         },
         Commit = {
           prompt = "実装差分に対するコミットメッセージを日本語で記述してください。",
-          mapping = "<leader>cco",
+          mapping = "<leader>ccO",
           description = "コミットメッセージの作成",
           selection = require("CopilotChat.select").gitdiff,
         },
         CommitStaged = {
           prompt = "ステージ済みの変更に対するコミットメッセージを日本語で記述してください。",
-          mapping = "<leader>cs",
+          mapping = "<leader>ccs",
           description = "ステージ済みのコミットメッセージの作成",
           selection = function(source)
             return require("CopilotChat.select").gitdiff(source, true)
