c05b0ae47ad2a1a498a7b5a8eaa54edc753d9a0c
nnamm <t.nnamm@gmail.com>
2026-01-12 15:12:40 +0900

    Copilot pro unsubscribe to reconfirm the necessity of my workflow.

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,22 +1,22 @@
 return {
-  -- "neovim/nvim-lspconfig",
-  -- opts = {
-  --   servers = {
-  --     gopls = {
-  --       settings = {
-  --         gopls = {
-  --           staticcheck = true,
-  --           analyses = {
-  --             unusedparams = true,
-  --           },
-  --           gofumpt = true,
-  --           memoryMode = "DegradeClosed",
-  --           expandWorkspaceToModule = true,
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      gopls = {
+        settings = {
+          gopls = {
+            staticcheck = true,
+            analyses = {
+              unusedparams = true,
+            },
+            gofumpt = true,
+            memoryMode = "DegradeClosed",
+            -- expandWorkspaceToModule = true,
+          },
+        },
+      },
+    },
+  },
 
   -- nvim-cmp
   -- Use the Tab-key to confirm a candidate
@@ -44,69 +44,71 @@ return {
   -- },
 
   -- copilot
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    opts = {
-      model = "claude-sonnet-4.5",
-      temperature = 0.1,
-      auto_fold = true,
-      -- window = {
-      --   layout = "float",
-      --   relative = "editor",
-      -- },
-      show_help = "yes",
-      prompts = {
-        Explain = {
-          prompt = "/COPILOT_EXPLAIN コードを日本語で説明してください",
-          mapping = "<leader>cce",
-          description = "コードの説明",
-        },
-        Review = {
-          prompt = "/COPILOT_REVIEW コードを日本語でレビューしてください。",
-          mapping = "<leader>ccr",
-          description = "コードのレビュー",
-        },
-        Fix = {
-          prompt = "/COPILOT_FIX このコードには問題があります。バグを修正したコードを表示してください。説明は日本語でお願いします。",
-          mapping = "<leader>ccf",
-          description = "バグコードの修正",
-        },
-        Optimize = {
-          prompt = "/COPILOT_REFACTOR 選択したコードを最適化し、パフォーマンスと可読性を向上させてください。説明は日本語でお願いします。",
-          mapping = "<leader>cco",
-          description = "コードの最適化",
-        },
-        Docs = {
-          prompt = "/COPILOT_GENERATE 選択したコードに関するドキュメントコメントを日本語で生成してください。",
-          mapping = "<leader>ccd",
-          description = "コードのドキュメント作成",
-        },
-        Tests = {
-          prompt = "/COPILOT_TESTS 選択したコードの詳細なユニットテストを書いてください。説明は日本語でお願いします。",
-          mapping = "<leader>cct",
-          description = "テストコード作成",
-        },
-        FixDiagnostic = {
-          prompt = "コードの診断結果に従って問題を修正してください。修正内容の説明は日本語でお願いします。",
-          mapping = "<leader>ccD",
-          description = "コードの修正",
-          selection = require("CopilotChat.select").diagnostics,
-        },
-        Commit = {
-          prompt = "実装差分に対するコミットメッセージを日本語で記述してください。",
-          mapping = "<leader>ccO",
-          description = "コミットメッセージの作成",
-          selection = require("CopilotChat.select").gitdiff,
-        },
-        CommitStaged = {
-          prompt = "ステージ済みの変更に対するコミットメッセージを日本語で記述してください。",
-          mapping = "<leader>ccs",
-          description = "ステージ済みのコミットメッセージの作成",
-          selection = function(source)
-            return require("CopilotChat.select").gitdiff(source, true)
-          end,
-        },
-      },
-    },
-  },
+  -- Stopping Copilot Pro.
+  -- Objective: to determine necessity.
+  --   {
+  --     "CopilotC-Nvim/CopilotChat.nvim",
+  --     opts = {
+  --       model = "claude-sonnet-4.5",
+  --       temperature = 0.1,
+  --       auto_fold = true,
+  --       -- window = {
+  --       --   layout = "float",
+  --       --   relative = "editor",
+  --       -- },
+  --       show_help = "yes",
+  --       prompts = {
+  --         Explain = {
+  --           prompt = "/COPILOT_EXPLAIN コードを日本語で説明してください",
+  --           mapping = "<leader>cce",
+  --           description = "コードの説明",
+  --         },
+  --         Review = {
+  --           prompt = "/COPILOT_REVIEW コードを日本語でレビューしてください。",
+  --           mapping = "<leader>ccr",
+  --           description = "コードのレビュー",
+  --         },
+  --         Fix = {
+  --           prompt = "/COPILOT_FIX このコードには問題があります。バグを修正したコードを表示してください。説明は日本語でお願いします。",
+  --           mapping = "<leader>ccf",
+  --           description = "バグコードの修正",
+  --         },
+  --         Optimize = {
+  --           prompt = "/COPILOT_REFACTOR 選択したコードを最適化し、パフォーマンスと可読性を向上させてください。説明は日本語でお願いします。",
+  --           mapping = "<leader>cco",
+  --           description = "コードの最適化",
+  --         },
+  --         Docs = {
+  --           prompt = "/COPILOT_GENERATE 選択したコードに関するドキュメントコメントを日本語で生成してください。",
+  --           mapping = "<leader>ccd",
+  --           description = "コードのドキュメント作成",
+  --         },
+  --         Tests = {
+  --           prompt = "/COPILOT_TESTS 選択したコードの詳細なユニットテストを書いてください。説明は日本語でお願いします。",
+  --           mapping = "<leader>cct",
+  --           description = "テストコード作成",
+  --         },
+  --         FixDiagnostic = {
+  --           prompt = "コードの診断結果に従って問題を修正してください。修正内容の説明は日本語でお願いします。",
+  --           mapping = "<leader>ccD",
+  --           description = "コードの修正",
+  --           selection = require("CopilotChat.select").diagnostics,
+  --         },
+  --         Commit = {
+  --           prompt = "実装差分に対するコミットメッセージを日本語で記述してください。",
+  --           mapping = "<leader>ccO",
+  --           description = "コミットメッセージの作成",
+  --           selection = require("CopilotChat.select").gitdiff,
+  --         },
+  --         CommitStaged = {
+  --           prompt = "ステージ済みの変更に対するコミットメッセージを日本語で記述してください。",
+  --           mapping = "<leader>ccs",
+  --           description = "ステージ済みのコミットメッセージの作成",
+  --           selection = function(source)
+  --             return require("CopilotChat.select").gitdiff(source, true)
+  --           end,
+  --         },
+  --       },
+  --     },
+  --   },
 }
