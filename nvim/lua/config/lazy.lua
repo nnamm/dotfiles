6abcf61dcbf45e6f2612362f097a3e47a6173fb2
nnamm <t.nnamm@gmail.com>
2026-07-27 08:49:30 +0900

    chore: new color theme settings

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "falcon" } },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.neogen" },
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,91 +1,106 @@
 return {
   -----------------------------------
+  --  active theme: ayu (mirage)   --
+  -----------------------------------
+  {
+    "Shatur/neovim-ayu",
+    lazy = true,
+    main = "ayu",
+    opts = {
+      mirage = true,
+      terminal = true,
+      -- overrides = function()
+      --   local colors = require("ayu.colors")
+      --   return { Comment = { fg = colors.comment, italic = true } }
+      -- end,
+      overrides = {},
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "ayu-mirage" },
+  },
+
+  -----------------------------------
   --  suggestions for dark themes  --
   -----------------------------------
-  -- "rose-pine/neovim",
-  -- name = "rose-pine",
-  -- opts = {
-  --   styles = { bold = true, italic = true, transparency = true },
-  --   palette = {
-  --     main = {
-  --       _nc = "#16141f",
-  --       base = "#191724",
-  --       surface = "#1f1d2e",
-  --       overlay = "#26233a",
-  --       muted = "#6e6a86",
-  --       subtle = "#908caa",
-  --       text = "#e0def4",
-  --       love = "#eb6f92",
-  --       -- gold = "#f6c177", -- original
-  --       gold = "#e6c087",
-  --       rose = "#ebbcba",
-  --       pine = "#31748f",
-  --       -- foam = "#9ccfd8", -- original
-  --       foam = "#c5e5ea",
-  --       iris = "#c4a7e7",
-  --       leaf = "#95b1ac",
-  --       highlight_low = "#21202e",
-  --       highlight_med = "#403d52",
-  --       highlight_high = "#524f67",
-  --       none = "NONE",
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   lazy = true,
+  --   opts = {
+  --     styles = { bold = true, italic = true, transparency = true },
+  --     palette = {
+  --       main = {
+  --         _nc = "#16141f",
+  --         base = "#191724",
+  --         surface = "#1f1d2e",
+  --         overlay = "#26233a",
+  --         muted = "#6e6a86",
+  --         subtle = "#908caa",
+  --         text = "#e0def4",
+  --         love = "#eb6f92",
+  --         -- gold = "#f6c177", -- original
+  --         gold = "#e6c087",
+  --         rose = "#ebbcba",
+  --         pine = "#31748f",
+  --         -- foam = "#9ccfd8", -- original
+  --         foam = "#c5e5ea",
+  --         iris = "#c4a7e7",
+  --         leaf = "#95b1ac",
+  --         highlight_low = "#21202e",
+  --         highlight_med = "#403d52",
+  --         highlight_high = "#524f67",
+  --         none = "NONE",
+  --       },
   --     },
+  --     highlight_groups = {},
   --   },
-  --   highlight_groups = {},
   -- },
 
-  -- "shaunsingh/nord.nvim",
-  -- lazy = false,
-  -- priority = 1000,
-  -- config = function()
-  --   vim.cmd([[colorscheme nord]])
-  --   vim.g.nord_contrast = true
-  --   vim.g.nord_borders = false
-  --   vim.g.nord_disable_background = true
-  --   vim.g.nord_cursorline_transparent = true
-  --   vim.g.nord_enable_sidebar_background = false
-  --   vim.g.nord_italic = true
-  --   vim.g.nord_uniform_diff_background = true
-  --   vim.g.nord_bold = true
-  --   require("nord").set()
-  -- end,
+  -- {
+  --   "shaunsingh/nord.nvim",
+  --   lazy = true,
+  --   init = function()
+  --     vim.g.nord_contrast = true
+  --     vim.g.nord_borders = false
+  --     vim.g.nord_disable_background = true
+  --     vim.g.nord_cursorline_transparent = true
+  --     vim.g.nord_enable_sidebar_background = false
+  --     vim.g.nord_italic = true
+  --     vim.g.nord_uniform_diff_background = true
+  --     vim.g.nord_bold = true
+  --   end,
+  -- },
 
-  -- "andersevenrud/nordic.nvim",
-  -- lazy = false,
-  -- priority = 1000,
-  -- config = function()
-  --   vim.g.nord_underline_option = "none"
-  --   vim.g.nord_italic = true
-  --   vim.g.nord_italic_comments = true
-  --   vim.g.nord_minimal_mode = false
-  --   vim.g.nord_alternate_backgrounds = false
-  --   vim.cmd("colorscheme nordic")
-  -- end,
+  -- {
+  --   "andersevenrud/nordic.nvim",
+  --   lazy = true,
+  --   init = function()
+  --     vim.g.nord_underline_option = "none"
+  --     vim.g.nord_italic = true
+  --     vim.g.nord_italic_comments = true
+  --     vim.g.nord_minimal_mode = false
+  --     vim.g.nord_alternate_backgrounds = false
+  --   end,
+  -- },
 
-  -- "cocopon/iceberg.vim",
-  -- lazy = false,
-  -- priority = 1000,
+  -- { "cocopon/iceberg.vim", lazy = true },
 
-  "fenetikm/falcon",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.g.falcon_inactive = 1
-    vim.g.falcon_background = 0
-    vim.cmd("colorscheme falcon")
-  end,
+  -- {
+  --   "fenetikm/falcon",
+  --   lazy = true,
+  --   init = function()
+  --     vim.g.falcon_inactive = 1
+  --     vim.g.falcon_background = 0
+  --   end,
+  -- },
 
   -----------------------------------
   --  suggestions for light theme  --
   -----------------------------------
-  -- "EdenEast/nightfox.nvim",
-  -- lazy = false,
-  -- priority = 1000,
-
-  -- "talha-akram/noctis.nvim",
-  -- lazy = false,
-  -- priority = 1000,
-
-  -- "savq/melange-nvim",
-  -- lazy = false,
-  -- priority = 1000,
+  -- { "EdenEast/nightfox.nvim", lazy = true },
+  -- { "talha-akram/noctis.nvim", lazy = true },
+  -- { "savq/melange-nvim", lazy = true },
 }
