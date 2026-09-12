afe8105aeb6b96775925a367ca183d0980a9e352
nnamm <t.nnamm@gmail.com>
2026-09-12 19:06:04 +0900

    新しいツール開発で使用（sqlは4文字が好み）

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -10,7 +10,7 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 
 -- Use 4-space indentation for these filetypes (buffer-local)
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "go", "c", "cpp", "glsl" },
+  pattern = { "go", "c", "cpp", "glsl", "sql" },
   callback = function()
     vim.opt_local.shiftwidth = 4
     vim.opt_local.tabstop = 4
